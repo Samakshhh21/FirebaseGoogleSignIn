2c4a386c75c092d4ddc11df946ec36e57fa4bbb3
samaksh <samakshsangwan@gmail.com>
2024-02-20 11:06:50 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/firebasesignin/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.firebasesignin
+
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private lateinit var mAuth: FirebaseAuth
+
+
+
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
+
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("147975249602-u0nv03pgm2aindts57umt58hm0ikived.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+
+
+        val textView = findViewById<TextView>(R.id.textView)
+
+        val auth = Firebase.auth
+        val user = auth.currentUser
+
+        if (user != null) {
+            val userName = user.displayName
+            textView.text = "Welcome, " + userName
+        } else {
+            // Handle the case where the user is not signed in
+        }
+
+
+
+// Inside onCreate() method
+        val sign_out_button = findViewById<Button>(R.id.button)
+        sign_out_button.setOnClickListener {
+            signOutAndStartSignInActivity()
+        }
+
+
+
+
+    }
+
+
+    private fun signOutAndStartSignInActivity() {
+        mAuth.signOut()
+
+        mGoogleSignInClient.signOut().addOnCompleteListener(this) {
+            // Optional: Update UI or show a message to the user
+            val intent = Intent(this@MainActivity, MainActivity2::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
+
